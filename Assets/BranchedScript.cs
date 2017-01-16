43d03db9ea613fbea5724e6ddabff796516b1b39
NedNedyalkov <nedvnedyalkov@gmail.com>
2017-01-16 18:47:32 +0200

    changes!

--- Assets/BranchedScript.cs
+++ Assets/BranchedScript.cs
@@ -1,15 +1,6 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class BranchedScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //Made some changes
 }
