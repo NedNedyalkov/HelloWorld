6b5caf8b0bafbdeeb6ba3153902c1dd2b55b0713
NedNedyalkov <nedvnedyalkov@gmail.com>
2017-01-16 18:45:04 +0200

    Made some changes

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -1,15 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class FirstScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class FirstScript : MonoBehaviour
+{
+    //Made some changes
 }
